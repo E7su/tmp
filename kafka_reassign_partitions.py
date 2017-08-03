c64be9ba13aad74e4a0354a66735a58b91094d0c
Etsu <etsu4296@gmail.com>
2017-08-03 17:10:32 +0300

    Update kafka_reassign_partitions.py

--- kafka_reassign_partitions.py
+++ kafka_reassign_partitions.py
@@ -1,11 +1,27 @@
 from kafka import KafkaProducer
 import json
-import random
-import json
+import ast
 
-# Datacenter's name: list of the brokers
-hosts_dict = {0: [0, 1, 2], 1: [3, 4, 5], 2: [6, 7, 8, 9, 10]}
-topics_list = ["__consumer_offsets", "partitioned-topic", "fast-messages", "summary-markers"]
+
+# Get topics list and brokers dict from file
+def get_topics_brokers_from_file():
+    # Datacenter's name: list of the brokers
+    bf = open('brokers_datacenters_list')
+    hosts_string = bf.readlines(1)[0]
+    bf.close()
+
+    # Magic to transform string representation of a Dictionary to a dictionary
+    hosts_dict = ast.literal_eval(hosts_string)
+    assert type(hosts_dict) is dict
+
+    # List of the topics
+    topics_list = []
+    tf = open('topics_for_reassign_list')
+    for topics in tf:
+        topics_list.append(topics[:-1])
+
+    return topics_list, hosts_dict
+
 
 default_json = """
 {
@@ -18,13 +34,16 @@ default_json = """
 }
 """
 
-parsed_string = json.loads(default_json)
-desc_of_each_part = parsed_string['partitions'][0].copy()
-parsed_string['partitions'].clear()
+
+def parse_json():
+    parsed_dict = json.loads(default_json)
+    desc_of_each_part = parsed_dict['partitions'][0].copy()
+    parsed_dict['partitions'].clear()
+    return parsed_dict, desc_of_each_part
 
 
 # Create json with value for entered topics
-def create_json_for_partitions():
+def create_json_for_partitions(topics_list, hosts_dict, parsed_dict, desc_of_each_part):
     inc = 0
     # For each topic:
     for topic in topics_list:
@@ -33,15 +52,17 @@ def create_json_for_partitions():
         # For each partition for this topic:
         for partitions_num in partitions_list:
             inc = inc + 1
+
             # Write the number of the partition
             desc_of_each_part['partition'] = partitions_num
             # Write the name of the topic without \n
             desc_of_each_part['topic'] = topic
             # Determine brokers for storage partitions for this topic
-            desc_of_each_part['replicas'] = create_extended_broker_list(inc)
+            desc_of_each_part['replicas'] = create_extended_broker_list(hosts_dict, inc)
             # Add new info to json
-            parsed_string['partitions'].append(desc_of_each_part.copy())
-    return parsed_string
+            parsed_dict['partitions'].append(desc_of_each_part.copy())
+
+    return parsed_dict
 
 
 # Get the list of the partitions for topic
@@ -52,8 +73,9 @@ def get_partitions_list(topic):
 
 
 # Generate new list of the brokers (for storage partitions for each topics on the new datacenters)
-def create_extended_broker_list(inc):
+def create_extended_broker_list(hosts_dict, inc):
     updated_broker_list = []
+
     for datacenter_num in hosts_dict:
         # List of the brokers in the current datacenter
         broker_list = hosts_dict[datacenter_num]
@@ -61,13 +83,15 @@ def create_extended_broker_list(inc):
         broker_num = inc % len(broker_list)
         current_broker = broker_list[broker_num]
         updated_broker_list.append(current_broker)
+
     return updated_broker_list
 
 
 def main():
-    list_for_json = create_json_for_partitions()
+    topics_list, hosts_dict = get_topics_brokers_from_file()
+    parsed_dict, desc_of_each_part = parse_json()
+    list_for_json = create_json_for_partitions(topics_list, hosts_dict, parsed_dict, desc_of_each_part)
     result_json = json.dumps(list_for_json)
-
     # Magic happens here to make it pretty-printed
     pretty_json = json.dumps(json.loads(result_json), indent=4, sort_keys=True)
     print(pretty_json)
